a8890a567014cbcaae7ef16db2a2f3c5b1bcf8a8
testpysoft <sg.sasha.dubovik@gmail.com>
2016-06-16 20:48:03 +0300

    calc

---
+++ calc.py
@@ -0,0 +1,32 @@
+print('Please input a')
+number_a = int(input())
+print('Please input b')
+number_b = int(input())
+print('Please input operand')
+oper=input()
+
+def add():
+    print(number_a+number_b)
+
+def multy():
+    print(number_a*number_b)
+
+def divide():
+    if (number_b== 0):
+        print('division by zero')
+        return
+    print(number_a/number_b)
+def multiply():
+    print(number_a*number_b)
+def subtract():
+    print (number_a-number_b)
+
+
+
+
+
+
+actions = {'add': add, 'multiply': multiply,'divide':divide,'subtract': subtract}
+
+actions[oper]()
+
