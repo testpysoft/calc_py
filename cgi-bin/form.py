d570f0834995ec1285bd9348b3a018cb89178b7c
testpysoft <sg.sasha.dubovik@gmail.com>
2016-06-21 17:09:58 +0300

    last divide

--- cgi-bin/form.py
+++ cgi-bin/form.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
 import cgi
 
-
 def add(x,y):
     return x+y
-def multiply():
-     return x*y
 def divide(x,y):
-    if (number_b== 0):
+    if (y == 0):
         print('division by zero')
         return
-    return a/b
-def multiply(x,y):
+    return x/y
+def multiply(x, y):
     return x*y
-def subtract(x,y):
+def subtract(x, y):
     return x-y
 
 form = cgi.FieldStorage()
-number_a = form.getfirst("number_a", "100")
-number_b= form.getfirst("number_b", "100")
+number_a = float(form.getfirst("number_a", "0"))
+  
+    
+
+number_b= float(form.getfirst("number_b", "0"))
 oper = form.getfirst("action", "add")
 
 actions = {'add': add, 'multiply': multiply,'divide':divide,'subtract': subtract}
-result=actions[oper](number_a,number_b)
+result=actions[oper](number_a, number_b)
 
 
 print("Content-type: text/html\n")
@@ -36,7 +36,7 @@ print("""<!DOCTYPE HTML>
 
 print("<h1>We all done!</h1>")
 print("<p> x: {}</p>".format(number_a))
-print("<p> action: {}</p>".format(oper))
+print("<p {}</p>".format(oper))
 print("<p> y: {}</p>".format(number_b))
 print("<p> result: {}</p>".format(result))
 print("""<p> <a href="/"><button>again computer    </button> </a></p>""")
